b9ad3008aa755b837c9bdebb8ef185007f8f5bfc
Mozzie <zhiying8710@hotmail.com>
2021-01-27 10:45:25 +0800

    Update nav.config.ts

--- nav.config.ts
+++ nav.config.ts
@@ -3,13 +3,13 @@ import { IConfig } from './src/types'
 // 网站 LOGO 替换根目录下的 logo.png， 务必为 png 后缀
 const c: IConfig = {
   // [必填], 请填写您的仓库地址
-  gitRepoUrl: 'https://github.com/xjh22222228/nav',
+  gitRepoUrl: 'https://github.com/zhiying8710/nav',
 
   // 路由是否Hash模式, 如果是部署在github pages 务必设为 true
   hashMode: true,
 
   // 您的网站地址，这对于SEO很重要
-  homeUrl: 'https://nav3.cn',
+  homeUrl: 'http://47.94.91.142:996',
 
   // 网站标题
   title: '发现导航 - 精选实用导航网站',
@@ -18,7 +18,7 @@ const c: IConfig = {
   description: '发现导航 - 精选实用导航网站',
 
   // 网站关键字
-  keywords: '导航,前端资源,社区站点,设计师,实用工具,学习资源,运营,网络安全,node.js',
+  keywords: '',
 
   // 默认主题: Light | Sim | Side
   theme: 'Light',
@@ -30,51 +30,17 @@ const c: IConfig = {
       name: '站内',
       icon: 'assets/logo.png',
       placeholder: '站内搜索'
-    },
-    {
-      name: '百度',
-      url: 'https://www.baidu.com/s?wd=',
-      icon: 'assets/engine/baidu.svg',
-      placeholder: '百度一下'
-    },
-    {
-      name: 'Google',
-      url: 'https://www.google.com/search?q=',
-      icon: 'assets/engine/google.svg',
-    },
-    {
-      name: '必应',
-      url: 'https://cn.bing.com/search?q=',
-      icon: 'assets/engine/bing.svg',
-    },
-    {
-      name: 'GitHub',
-      url: 'https://github.com/search?q=',
-      icon: 'assets/engine/github.svg',
-      placeholder: 'Search GitHub'
-    },
-    {
-      name: '知乎',
-      url: 'https://www.zhihu.com/search?type=content&q=',
-      icon: 'assets/engine/zhihu.svg',
-    },
-    {
-      name: '豆瓣',
-      url: 'https://search.douban.com/book/subject_search?search_text=',
-      icon: 'assets/engine/douban.svg',
-      placeholder: '书名、作者、ISBN'
     }
   ],
 
   // 网站底部内容, 版权信息、备案号, 可以是 HTML
   footerContent: `
     <div style="font-weight: bold;">共收录\${total}个网站</div>
-    <div>Copyright © 2018-2021 nav3.cn, All Rights Reserved</div>
   `,
 
   // 百度统计地址
   // https://tongji.baidu.com/web/welcome/login
-  baiduStatisticsUrl: 'https://hm.baidu.com/hm.js?4582be7af7e7c95ef75351e07c6c32ba',
+  baiduStatisticsUrl: '',
 
   // CNZZ 统计地址
   // https://www.cnzz.com/o_index.php
